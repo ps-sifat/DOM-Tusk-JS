e9768813bb71972342e47009c028e6c69bff7109
sifatul-islam-bd <mdsifatulislam2357@gmail.com>
2024-05-20 23:44:36 +0600

    js add

--- Counter/js/main.js
+++ Counter/js/main.js
@@ -1,8 +1,18 @@
 const counter = document.querySelector(".counter");
 const btn = document.querySelectorAll(".btn");
 
+let count = 0;
 btn.forEach((btn) => {
-  btn.addEventListener(`click`, () => {
-    console.log("click done");
+  btn.addEventListener("click", (eachBtn) => {
+    let className = eachBtn.target.className;
+
+    if (className.includes("Incress")) {
+      count++;
+    } else if (className.includes(`Decress`)) {
+      count--;
+    } else {
+      count = 0;
+    }
+    counter.innerHTML = count;
   });
 });
